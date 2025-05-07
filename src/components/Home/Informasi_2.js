29506e7ae8eaa773283c85250f232242235ef913
Benediktus Mikael Auwdinata <152288822+Huntbase@users.noreply.github.com>
2025-05-07 09:21:41 +0700

    Perbaikan Warna

--- src/components/Home/Informasi_2.js
+++ src/components/Home/Informasi_2.js
@@ -4,23 +4,24 @@ import "./css/Informasi_2.css";
 const Informasi = () => {
   return (
     <div className="container">
+      <h1 className="title">Daftar Mart</h1>
+
       <div className="overlay">
-        <h1 className="title">Daftar Mart</h1>
         <div className="stats">
           <div className="stat">
             <p className="number">519</p>
-            Sebanyak <strong>519 mini market</strong> siap melayani kebutuhan
-            Anda di seluruh penjuru Bogor.
+            Sebanyak <b>519 mini market</b> siap melayani kebutuhan Anda di
+            seluruh penjuru Bogor.
           </div>
           <div className="stat">
-            <p className="number">?</p>
-            Bogor juga memiliki berbagai <strong>pasar modern</strong> yang
-            menawarkan pengalaman belanja nyaman dan lengkap.
+            <p className="number">513</p>
+            Bogor juga memiliki berbagai <b>513 pasar modern</b> yang menawarkan
+            pengalaman belanja nyaman dan lengkap.
           </div>
           <div className="stat">
             <p className="number">14</p>
-            Kota Bogor memiliki <strong>14 pasar tradisional</strong> yang kaya
-            akan budaya dan keanekaragaman produk lokal.
+            Kota Bogor memiliki <b>14 pasar tradisional</b> yang kaya akan
+            budaya dan keanekaragaman produk lokal.
           </div>
         </div>
       </div>
