390de6dda4c8356883954c1c0540c85ef60ef34a
Benediktus Mikael Auwdinata <152288822+Huntbase@users.noreply.github.com>
2025-06-19 10:01:43 +0700

    Perbaikan footer

--- src/components/pages/FAQ.js
+++ src/components/pages/FAQ.js
@@ -1,17 +1,19 @@
 import React from "react";
-import Navbar from "../Navbar&Footer/Navbar.js";
-import FAQ from "../FAQ/FAQ.js";
-import Footer from "../Navbar&Footer/Footer.js";
+import Navbar from "../Navbar&Footer/Navbar";
+import FAQ from "../FAQ/FAQ";
+import Footer from "../Navbar&Footer/Footer";
 import "../Home/css/App.css";
 
-const isi = () => {
+const Isi = () => {
   return (
-    <>
+    <div className="page-container">
       <Navbar />
-      <FAQ />
+      <main className="content-wrap">
+        <FAQ />
+      </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
-export default isi;
+export default Isi;
---
+++ src/firebase/UploadPasar.js
@@ -0,0 +1,100 @@
+import { db } from "./firebaseConfig";
+import { setDoc, doc } from "firebase/firestore";
+
+export const pasarData = [
+  {
+    id: 1,
+    nama: "Pasar Bogor",
+    lokasi: "Jalan Suryakencana 18216",
+    kota: "Bogor",
+    gambar:
+      "https://static.republika.co.id/uploads/images/inpicture_slide/036481400-1688106898-830-556.jpg",
+    JamOperasional: "07:00 - 11:00",
+    GoogleMaps: "https://maps.app.goo.gl/3gJkV3D2r82WHv45A",
+    type: "pasar",
+  },
+  {
+    id: 2,
+    nama: "Pasar Anyar",
+    lokasi: "Jalan Dewi Sartika No. 10",
+    kota: "Bogor",
+    gambar:
+      "https://tangerangekspres.disway.id/upload/48981b065e437a0138c94750365d01b5.jpeg",
+    JamOperasional: "06:00 - 12:00",
+    GoogleMaps: "https://maps.app.goo.gl/SGdtPchbj6MyiQ2A6",
+    type: "pasar",
+  },
+  {
+    id: 3,
+    nama: "Pasar Ciawi",
+    lokasi: "Jalan Raya Ciawi No. 15",
+    kota: "Bogor",
+    gambar: "https://bogor-kita.com/wp-content/uploads/2022/03/pasar-ciawi.jpg",
+    JamOperasional: "05:30 - 13:00",
+    GoogleMaps: "https://maps.app.goo.gl/SwAbqHMTHFih4hp17",
+    type: "pasar",
+  },
+  {
+    id: 4,
+    nama: "Pasar Kebon Kembang",
+    lokasi: "Jalan Merdeka No. 45",
+    kota: "Bogor",
+    gambar:
+      "https://lovelybogor.com/wp-content/uploads/2015/11/Pasar-Tradisional-Di-Kota-Bogor-Pasar-Kebon-Kembang.jpg",
+    JamOperasional: "06:00 - 14:00",
+    GoogleMaps: "https://maps.app.goo.gl/3amRXjE3egHGG9xH8",
+    type: "pasar",
+  },
+  {
+    id: 5,
+    nama: "Pasar Cibinong",
+    lokasi: "Jalan Raya Cibinong No. 20",
+    kota: "Bogor",
+    gambar:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEitl0wYqgt38XtHEvCkM5isePn4cOJeYvDFZeHi0WQP7Cj_903gbsOopwKwyDVU2W__XAiFAfIWvb4NELYCQ0nkTFG1A0wsBwVXQwkddz42sV11vuee_eqiZsMtW7_zjHxRmHKnQYoo86s/s1600/IMG_20170517_172258.jpg",
+    JamOperasional: "05:00 - 13:00",
+    GoogleMaps: "https://maps.app.goo.gl/iZbbfsQ1s9D7Wfr69",
+    type: "pasar",
+  },
+  {
+    id: 6,
+    nama: "Pasar Gunung Batu",
+    lokasi: "Jalan Gunung Batu No. 7",
+    kota: "Bogor",
+    gambar:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRth-PILiBmjIndzJNYbJKarObiNA7u7KQnvQ&s",
+    JamOperasional: "06:30 - 12:30",
+    GoogleMaps: "https://maps.app.goo.gl/8qdfhdPAmXcF582b6",
+    type: "pasar",
+  },
+  {
+    id: 7,
+    nama: "Pasar Jambu Dua",
+    lokasi: "Jalan Pajajaran No. 100",
+    kota: "Bogor",
+    gambar:
+      "https://asset.kompas.com/crops/y2HXxeJEcE5KmjK2o_Kw_xoCZR8=/0x0:0x0/1200x800/data/photo/2024/06/27/667cf8925648a.jpeg",
+    JamOperasional: "07:00 - 11:30",
+    GoogleMaps: "https://maps.app.goo.gl/TuhPiTQLdH31Xb6v5",
+    type: "pasar",
+  },
+  {
+    id: 8,
+    nama: "Pasar Sindangbarang",
+    lokasi: "Jalan Sindangbarang No. 25",
+    kota: "Bogor",
+    gambar: "https://i.imgur.com/TeJ7iEd.png",
+    JamOperasional: "05:30 - 12:00",
+    GoogleMaps: "https://maps.app.goo.gl/aEsCZtubech2szjF8",
+    type: "pasar",
+  },
+];
+
+export async function uploadPasar() {
+  try {
+    await setDoc(doc(db, "pasar", pasarData.id.toString()), pasarData);
+    console.log("✅ Data pasar berhasil diunggah ke Firestore!");
+  } catch (error) {
+    console.error("❌ Gagal mengunggah:", error);
+  }
+}
